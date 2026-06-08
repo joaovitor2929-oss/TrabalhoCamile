ab162e49e94589c90e59412dcb1124577565e5bf
joaovitor2929-oss <joaonegao2929@gmail.com>
2026-06-07 21:53:18 -0300

    Segunda versão

--- quiz.c
+++ quiz.c
@@ -13,12 +13,13 @@ typedef struct Questoes{
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
+    FILE *banco;
     QUESTOES quiz[TAM];
     int editar = 0;
     int pontuação = 0;
     for(int i = 0; i < TAM; i++){
         printf("\n%d° pergunta:\n", i + 1);
-        scanf(" %49[^\n]", &quiz[i].pergunta);
+        scanf(" %49[^\n]", quiz[i].pergunta);
         
         printf("\nalternativaA:\n");
         scanf(" %19[^\n]", quiz[i].alternativaA);
@@ -30,22 +31,40 @@ int main()
         scanf(" %c", &quiz[i].gabarito);
         quiz[i].gabarito = toupper(quiz[i].gabarito);
     }
-
     
-    for(int i = 0; i < TAM; i++){
+    do{
+        printf("Editar pergunta: \n");
+        printf("0 para NÃO\n1 para SIM\n");
+        scanf("%d", &editar);
+    }while(editar != 0 && editar != 1);
+    
+    if(editar == 1){
+        for(int i = 0; i < TAM; i++){
         printf("\n%d° pergunta:\n", i + 1);
-        printf(" %s\n", quiz[i].pergunta);
+        scanf(" %49[^\n]", quiz[i].pergunta);
+        }
+    }
+    
+    banco = fopen("C:\\Users\\Samsung\\Desktop\\Questoes.txt", "w");
+    for(int j = 0; j < TAM; j++){
+        fprintf(banco, " %s\n", quiz[j].pergunta);
+    } 
+    fclose(banco);
+    
+    for(int k = 0; k < TAM; k++){
+        printf("\n%d° pergunta:\n", k + 1);
+        printf(" %s\n", quiz[k].pergunta);
         
         printf("\nalternativa A:\n");
-        printf(" %s\n", quiz[i].alternativaA);
+        printf(" %s\n", quiz[k].alternativaA);
         
         printf("\nalternativa B:\n");
-        printf(" %s\n", quiz[i].alternativaB);
+        printf(" %s\n", quiz[k].alternativaB);
         
         printf("\nSua respo (A ou B):\n");
-        scanf(" %c", &quiz[i].res);
-        quiz[i].res = toupper(quiz[i].res);
-        if(quiz[i].gabarito == quiz[i].res){
+        scanf(" %c", &quiz[k].res);
+        quiz[k].res = toupper(quiz[k].res);
+        if(quiz[k].gabarito == quiz[k].res){
             pontuação++;
         }
     }
